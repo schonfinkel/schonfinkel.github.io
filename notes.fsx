4ae3f9e5d3d384c43108b3088149983e5031037a
Marcos Benevides <marcos.schonfinkel@gmail.com>
2025-05-19 18:08:45 -0400

    org: update cv and add more notes

--- notes.fsx
+++ notes.fsx
@@ -2,23 +2,9 @@
 
 open System
 
-let createNote title (fileName: String) =
-    let createdAt = fileName.[0..13]
-    let date = $"{createdAt.[0..3]}-{createdAt.[4..5]}-{createdAt.[6..7]}"
-    let file = fileName.Replace(".org", "")
-
-    $"""
-    <div class="stub">
-      <h2>
-        <a href="./notes/{file}.html"> {title} </a>
-      </h2>
-      <small>{date}</small>
-    </div>
-    """
-
 let org =
     Shared.notes
-    |> List.map (fun (title, fileName) -> createNote title fileName)
+    |> List.map (fun (title, fileName) -> Shared.createNote title fileName)
     |> (fun s -> String.Join("\n", s))
 
 printfn "%s" org
--- posts.fsx
+++ posts.fsx
@@ -2,27 +2,13 @@
 
 open System
 
-let createPost title (fileName: String) =
-    let createdAt = fileName.[0..7]
-    let date = $"{createdAt.[0..3]}-{createdAt.[4..5]}-{createdAt.[6..7]}"
-    let file = fileName.Replace(".org", "")
-
-    $"""
-    <div class="stub">
-      <h2>
-        <a href="./blog/{file}.html"> {title} </a>
-      </h2>
-      <small>{date}</small>
-    </div>
-    """
-
 let filterNames (n: String) =
     n.Contains("draft") || n.Contains("rss") |> not
 
 let org =
     Shared.posts
     |> List.filter (fun (_title, fileName) -> filterNames (fileName))
-    |> List.map (fun (title, fileName) -> createPost title fileName)
+    |> List.map (fun (title, fileName) -> Shared.createPost title fileName)
     |> (fun s -> String.Join("\n", s))
 
 printfn "%s" org
